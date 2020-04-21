2f7b80eaf2105f92e77b812c6636ad20b6a161ac
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-21 22:52:10 +0200

    changed index to date

--- Queue_page/test.php
+++ Queue_page/test.php
@@ -11,18 +11,19 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT * FROM test_table";
+    $sql = "SELECT * FROM test_table ORDER BY time_entered DESC LIMIT 10";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         echo "<table>";
-        echo "<tr> <th>First name</th> <th>Last name</th> <th>Index</th> </tr>";
+        echo "<tr> <th>First name</th> <th>Last name</th> <th>Apply date</th> </tr>";
         while($row = $result->fetch_assoc()) {
 
-            echo "<tr> <td>" . $row['name'] . "</td> <td>" . $row['lastname'] . "</td> <td>" . $row['id'] . "</td> </tr>";
+            echo "<tr> <td>" . $row['name'] . "</td> <td>" . $row['lastname'] . "</td> <td>" . $row['time_entered'] . "</td> </tr>";
         }
         echo "</table>";
+
     } else {
         echo "0 results";
     }

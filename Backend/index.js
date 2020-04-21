649fdb25441350846eec189d4c923e584e9d31e9
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-21 11:37:50 +0200

    we can now read from a mysql database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+let mysql = require('mysql');
+let name = "";
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "exercises"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM exam", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result[0].first_name);
+      name = result[0].first_name;
+    });
+  });
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.send(`<h1>Hello World! ${name}</h1>`);
 });
 
 const port = process.env.port || 5000;

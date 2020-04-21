2bbd9fa7d0e515984d4f14986509b9a73ca71046
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-21 11:00:26 +0200

    Hello World in Express JS

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

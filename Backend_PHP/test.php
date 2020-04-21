b85a5e9448aa9c0a715605a3bd6c7d3768a6e89b
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-21 14:27:44 +0200

    created a simple webpage to read from database

--- Backend_PHP/test.php
+++ Backend_PHP/test.php
@@ -1,3 +1,26 @@
 <?php
-    echo rand(10,99);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "exercises";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT first_name FROM exam";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo $row['first_name'] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
 ?>

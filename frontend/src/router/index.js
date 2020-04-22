59c2fe6f4e007b6f71a5b4ef841a493d39dbd1e8
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-22 19:21:29 +0200

    updated navigation bar

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
+import Queue from '../views/QueueScreen.vue'
 import SignIn from '../views/SignIn.vue'
 import Register from '../views/Register.vue'
 
@@ -20,6 +21,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/queue',
+    name: 'queue',
+    component: Queue
   },
   {
     path: '/dashboard',

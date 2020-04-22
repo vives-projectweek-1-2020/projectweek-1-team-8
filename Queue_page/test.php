eae00c0565d65aa23860479dd8f1e8589f694ef2
Dylan Missuwe <dylan.missu@gmail.com>
2020-04-22 10:27:42 +0200

    added color coding to the rows in the queue

--- Queue_page/test.php
+++ Queue_page/test.php
@@ -11,16 +11,19 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT * FROM test_table ORDER BY time_entered DESC LIMIT 10";
+    $sql = "SELECT * FROM test_table WHERE state != 'not_in_queue' ORDER BY time_entered DESC LIMIT 20";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        echo "<table>";
+        echo "<table id='queue'>";
         echo "<tr> <th>First name</th> <th>Last name</th> <th>Apply date</th> </tr>";
         while($row = $result->fetch_assoc()) {
-
-            echo "<tr> <td>" . $row['name'] . "</td> <td>" . $row['lastname'] . "</td> <td>" . $row['time_entered'] . "</td> </tr>";
+            if ($row['state'] == "in_shop"){
+                echo "<tr style='background-color: lightgreen;'> <td>" . $row['name'] . "</td> <td>" . $row['lastname'] . "</td> <td>" . $row['time_entered'] . "</td> </tr>";
+            } else if ($row['state'] == "in_queue"){
+                echo "<tr style='background-color: lightsalmon;'> <td>" . $row['name'] . "</td> <td>" . $row['lastname'] . "</td> <td>" . $row['time_entered'] . "</td> </tr>";
+            }
         }
         echo "</table>";
 

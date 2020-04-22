dc387f5c13ea02de05dad348ee78675d882044d5
Wout Peirens <wout.peirens@gmail.com>
2020-04-22 21:37:26 +0200

    change navbar

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,53 +1,46 @@
 <template>
-    <div class="navigationscreen">
-        <ul>
-            <li>
-                <router-link :to="{ name: 'home' }" >
-                    Home
-                </router-link>
-            </li>
-            <li>
-                <router-link :to="{ name: 'signin' }" >
-                    Sign in
-                </router-link>
-            </li>
-            <li>
-                <a href ="#">
-                    Sign out
-                </a>
-            </li>
-            <li>
-                Hello, *username comes here*
-            </li>
-            <li>
-                <router-link :to="{ name: 'dashboard' }" >
-                    Dashboard
-                </router-link>
-            </li>
-        </ul>
+    <div>
+        <nav class="navbar"
+            <a>
+                Welcome *username comes here*!
+            </a>
+            <router-link :to="{ name: 'home' }" >
+                Home
+            </router-link>
+            <router-link :to="{ name: 'signin' }" >
+                Sign in
+            </router-link>
+            <router-link :to="{ name: '#'}">
+                Sign out
+            </router-link>
+            <router-link :to="{ name: 'dashboard' }" >
+                Dashboard
+            </router-link>
+            <router-link :to="{ name: 'queue' }" >
+                Queue
+            </router-link>
+        </nav>
     </div>
 </template>
 
 <style scoped>
 
 .router-link-exact-active {
-  color: red;
+  color: #42b983;
 }
-.navigationscreen{
-    background-image: linear-gradient(253deg,#3ba5b4 0,#38ba8a 100%);
-    list-style-type: none;
-    margin: 20px;
-    padding: 80px 10px;
-    width: 30%;
-    min-width: 240px;
+.navbar {
+    overflow: hidden;
+    background-color: #303030;
 }
-li{
-    display: inline;
-    margin: 10px;
+.navbar > * {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
 }
-ul{
-    margin: 0px;
-    padding-inline-start: 0px;
+.navbar > *:hover {
+    color: lightsalmon;
 }
-
 </style>

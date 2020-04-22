fa5acc047483e860fc04e96691caaec4d1cfd192
Wout Peirens <wout.peirens@gmail.com>
2020-04-22 13:21:16 +0200

    made pagestatus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <statuspage v-bind="statuspage" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import statuspage from './components/statuspage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    statuspage
   }
 }
 </script>

aa416760f055277795c076ee89019c8553eabfd6
Arno Baecke <arno.baecke@gmail.com>
2020-04-22 15:41:20 +0200

    Working on login-screen with api

--- frontend/src/components/SignInScreen.vue
+++ frontend/src/components/SignInScreen.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="login-box">
+  <div class="signin-box">
     <img alt="WAITSMART" src="../assets/logo.png">
     <h2>Please log in to continue</h2>
     {{ form }}
-    <form action="#" @submit.prevent="login">
+    <form action="#" @submit.prevent="signin">
       <p>Username</p>
       <input id="email" v-model="form.email" type="email" placeholder="Please enter your email." />
       <p>Password</p>
       <input id="password" v-model="form.password" type="password" placeholder="Please enter your password." />
       <br><br>
-      <button type="submit" @click="login">Sign in</button>
+      <button type="submit" @click="signin">Sign in</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       form: {
-        username: '',
+        email: '',
         password: ''
       }
     }
   },
   methods: {
-    login () {
-      this.$store.dispatch('login', {
-        username: this.username,
-        password: this.password
-      })
+    async signin () {
+      const response = axios.post('http://localhost:8000/api/auth/signin')
+
+      console.log(response.data)
     }
   }
 }
@@ -37,7 +38,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.login-box {
+.signin-box {
     width: 30%;
     min-width: 240px;
     background-image: linear-gradient(253deg,#3ba5b4 0,#38ba8a 100%);
@@ -60,7 +61,7 @@ button {
     cursor: pointer;
 }
 
-.login-box img {
+.signin-box img {
     max-width: 80%;
 }
 </style>

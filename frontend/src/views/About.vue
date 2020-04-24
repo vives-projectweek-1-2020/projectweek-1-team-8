15738a855c01e3a226f564d8ea753e465e95b1d8
Timon Claerhout <timon.claerhout@student.vives.be>
2020-04-24 13:15:21 +0200

    finished about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="about">
   <br><br>
-    <img alt="WAITSMART" src="../assets/logo.png"/>
+    <img id="waitsmartlogo" alt="WAITSMART" src="../assets/logo.png"/>
     <h1>ABOUT US</h1>
-    <hr id="line">
     <p id="firstp">Hi! We are 4 students of highschool Vives that develloped a brandnew app called Waitsmart.<br>
     Its a free app that will help people and bussinesses to prevent long queues. So loses the costumers <br>
     no time and its much safer because you don't get close to other people. </p>
-    <p><b>How does it work?</b> With a few clicks in our app can you select a bussiness or even multiple bussinesses <br>
+    <p> <b>How does it work?</b> With a few clicks in our app can you select a bussiness or even multiple bussinesses <br>
     with a date and time you want. Our app will make sure that the bussiness get the reservation, if there are  <br>
     costumers before you then there will be a digital queue. On the app that gives you always the realtime <br>
     information of the queue you will see which place you're currently at. </p>
@@ -15,7 +14,14 @@
 </template>
 
 <style>
-#line {
-  width: 50%;
+#waitsmartlogo {
+  margin-left: 40%;
+}
+.about {
+  height: calc(100vh - 60px);
+  background-image: url('../assets/background1.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: grid;
 }
 </style>
